9068e9276971e6ad72b3b3d3691a701873bfc7ae
tejas122125 <tejasweekumarsingh@gmail.com>
2024-07-22 00:03:04 +0530

    trejas

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,4 +1,6 @@
 import cv2
+import plotly.graph_objects as go
+
 
 def read_video(video_path):
     cap = cv2.VideoCapture(video_path)
@@ -16,3 +18,40 @@ def save_video(ouput_video_frames,output_video_path):
     for frame in ouput_video_frames:
         out.write(frame)
     out.release()
+    
+    
+    
+
+def max_speed_guage(value,threshhold =20, title = "Max Speed Meter"):
+    
+    fig = go.Figure(go.Indicator(
+        mode="gauge+number+delta",
+        value=value,
+        title={'text': title, 'font': {'size': 24}},
+        delta={'reference': threshhold, 'increasing': {'color': "green"}},
+        gauge={
+            'axis': {'range': [0, 100], 'tickwidth': 1, 'tickcolor': "darkblue"},
+            'bar': {'color': "#677AF6"},
+            'bgcolor': "white",
+            'borderwidth': 2,
+            'bordercolor': "gray",
+            'steps': [
+                {'range': [0, 10], 'color': 'red'},
+                {'range': [10, 20], 'color': 'orange'},
+                {'range': [20, 30], 'color': 'yellow'},
+                {'range': [30, 40], 'color': 'lightgreen'},
+                {'range': [40, 70], 'color': 'green'}],
+            'threshold': {
+                'line': {'color': "#DF5E14", 'width': 4},
+                'thickness': 0.75,
+                'value': threshhold}}))
+
+    fig.update_layout(
+        # title={'text': title, 'font': {'size': 24}}
+        font={'color': "darkblue", 'family': "Arial"},
+                paper_bgcolor="#0A0F2F",
+
+        plot_bgcolor="white")
+
+    # fig.show()
+    return fig    
